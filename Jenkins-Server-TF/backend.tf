99c7a98b6f5115cdbdb9bb6775e33f86a71b8995
narasimha-epi <narasimha.jetty@extrapreneursindia.com>
2024-05-01 08:29:10 +0530

    updated terraform scripts

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    bucket = "testmldkancdcn"
+    region = "ap-south-1"
+    key    = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    # dynamodb_table = "Lock-Files"
+    encrypt = true
   }
   required_version = ">=0.13.0"
   required_providers {
